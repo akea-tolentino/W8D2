b16300d30ffbdd04ab994982612de114532f402b
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-16 17:30:11 -0700

    javascript practice

--- practice-for-ch-js-intro-js-exercises-long-practice/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice/phase_1_arrays.js
@@ -1,1 +1,54 @@
 // Your code here
+
+Array.prototype.uniq = function () {
+    let uniqArray = [];
+    this.forEach(uniqInclude);
+
+    function uniqInclude(ele) {
+        if (!uniqArray.includes(ele)) {
+            uniqArray.push(ele);
+        }
+    }
+    return uniqArray;
+};
+
+// Array.prototype.twoSum = function () {
+//     let sumsArray = [];
+//     for (let i = 0; i < this.length - 1; i++) {
+//         for (let j = 1; j < this.length - 1; j++) {
+//             if (this[i] + this[j] === 0) {
+//                 sumsArray.push([i, j]);
+//             }
+//         }
+//     }
+//       return sumsArray;  
+// };
+
+Array.prototype.twoSum = function () {
+    let sumsArray = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0 && j > i) {
+                let pair = [];
+                pair.push(i);
+                pair.push(j);
+                sumsArray.push(pair);    
+            }
+        }
+    }
+      return sumsArray;  
+};
+
+Array.prototype.transpose = function () {
+    const rows = this.length, col = this[0].length;
+    const grid = [];
+    for (let j = 0; j < this.length; j++) {
+        grid[j] = new Array(rows);
+    }
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            grid[j][i] = this[i][j];
+        }
+    }
+    return grid;
+}
--- practice-for-ch-js-intro-js-exercises-long-practice/phase_2_enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice/phase_2_enumerable.js
@@ -1,1 +1,42 @@
 // Your code here
+
+Array.prototype.myEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+       callback(this[i]);
+    }
+};
+
+Array.prototype.myMap = function (cb) {
+    let mapped = [];
+    this.myEach(ele => {
+        mapped.push(cb(ele));
+    })
+    return mapped;
+
+};
+
+function xfive(ele) {
+    return ele * 5;
+};
+
+// Array.prototype.myReduce = function (callback, acc, el) {
+//     acc = acc || this[0];
+//     let sum = 0;
+//     if (acc === this[0]){
+//         sum -= this[0];
+//     }
+//     this.myEach(acc=> {
+//         sum += el;
+//     })
+//     return sum;
+// };
+
+
+Array.prototype.myReduce = function (callback, initialValue) {
+   let acc = initialValue
+
+   this.myEach(acc =>
+    function callback(acc, el) {
+
+    })
+};
